c12b3d0a163091595734ad956821c5d23c1e9400
dedy akwety <82807040+dedyakwety@users.noreply.github.com>
2021-05-27 02:33:45 +0200

    Delete dedy-akwety directory

--- dedy-akwety/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$router = new Altorouter();
-$router->map('GET', '/', function(){
-    require_once 'Public/Pages/homePage.php';
-});
-$router->map('GET', '/curriculumVitae', function(){
-    require_once 'Public/Pages/curriculumVitae.php';
-});
-?>
